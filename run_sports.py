d37dce1e1e6d9898ccb813ba1770659817b4f405
Rodrigo <rodrigo4zevedo@gmail.com>
2018-03-12 16:49:50 -0300

    Added run nell sports

---
+++ run_sports.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 15:19:50 2018
+
+@author: 317005
+"""
+
+from probfoil.probfoil import *
+
+targets= ['athleteplaysinleague',
+'teamplaysinleague', 
+'athleteplaysforteam',
+'teamalsoknownas', 
+'athleteledsportsteam', 
+'teamplaysagainstteam', 
+'teamplayssport',
+'athleteplayssport']
+
+for target in targets:
+    for fold in range(1,6):
+        files = ['../rule_learning_experiment/probfoil/'+target+'/sports.settings', '../rule_learning_experiment/probfoil/'+target+'/sports_fold_'+str(fold)+'.data']
+        probfoil(files=files, l=5, verbose=2, symmetry_breaking=True)
+        
+print('Finished.')
