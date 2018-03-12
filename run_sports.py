2f4cd375d95879ef1efa2611134ebbd5926f8070
Rodrigo <rodrigo4zevedo@gmail.com>
2018-03-12 17:21:22 -0300

    Changed order in run sports

--- run_sports.py
+++ run_sports.py
@@ -16,8 +16,8 @@ targets= ['athleteplaysinleague',
 'teamplayssport',
 'athleteplayssport']
 
-for target in targets:
-    for fold in range(1,6):
+for fold in range(1,6):
+    for target in targets:
         files = ['../rule_learning_experiment/probfoil/'+target+'/sports.settings', '../rule_learning_experiment/probfoil/'+target+'/sports_fold_'+str(fold)+'.data']
         probfoil(files=files, l=5, verbose=2, symmetry_breaking=True)
         
